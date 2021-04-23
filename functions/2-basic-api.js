c04120d051dadf32d9bd74571075cd8475490974
chris Chen <59293007+githubucl@users.noreply.github.com>
2021-04-23 21:18:47 +1200

    finished basic hello world and basic api

---
+++ functions/2-basic-api.js
@@ -0,0 +1,10 @@
+// domain/.netlify/functions/2-basic-api
+
+const items = require('../assets/data')
+
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(items)
+    }
+}
